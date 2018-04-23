30f2f58d0053ce233d603d7a88a00b16711a3b3b
xmc125 <xmc125@21cn.com>
2018-04-23 10:17:21 +0800

    commit the code

---
+++ src/main/java/com/biaoyue/testdemo/App.java
@@ -0,0 +1,13 @@
+package com.biaoyue.testdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
